13ad4b501ac86708064abb36e0e259ff732f583a
Dartrix137 <daniel.ftg@hotmail.com>
2021-11-06 15:47:44 -0500

    Nuevos cambios

--- Cliente/login-corte3/src/Login/Login.jsx
+++ Cliente/login-corte3/src/Login/Login.jsx
@@ -1,14 +1,59 @@
 import React, { Fragment,useState , useEffect } from 'react';
 import { NavLink} from 'react-router-dom';
 import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+import Indicador from '../components/Indicador'
+import axios from 'axios';
 import "./Login.css";
 function Login() {
     const [modal, setModal]=useState (false);
+    const [nombreusuario, setNombreusuario]=useState('');
+    const [contraseña, setContraseña]=useState('');
+    const [loginnombreusuario, setLoginnombreusuario]=useState('');
+    const [logincontraseña, setLogincontraseña]=useState('');
+    const [loginstatus, setLoginstatus]=useState('');
+    const [estado, setEstado]=useState(false);
+    axios.defaults.withCredentials=true;
+    const Registro=()=>{
+        axios.post('http://localhost:3001/registro-usuario', {
+            nombre: nombreusuario, 
+            contraseña: contraseña
+        }).then((response)=>{
+            setModal(false);
+            console.log(response);
+        });
+    };
+
+    const Login=()=>{
+        axios.post('http://localhost:3001/login', {
+            nombre: loginnombreusuario, 
+            contraseña: logincontraseña
+        }).then((response)=>{
+            if(response.data.Mensaje)
+            {
+                setLoginstatus(response.data.Mensaje);
+                setEstado(false);
+            }else{
+                setLoginstatus(response.data.nombre);
+            }
+        });
+    };
+
+    useEffect(()=>{
+        axios.get('http://localhost:3001/login').then((response)=>{
+            if(response.data.loggedIn== true){
+                setLoginstatus(response.data.usuario[0].nombre);
+                setEstado(true);
+            }
+        });
+    },[]);
+
     return (
         <Fragment>
             <header>
                 <nav id="titulo" className="navbar text-center d-flex justify-content-center">
                     <h1 className="d-flex justify-content-center">Login</h1>
+                    <h1 className="d-flex justify-content-center">{loginstatus}</h1>
+                    <indicadorpassword/>
                 </nav>
             </header>
             <div className="container-fluid">
@@ -17,15 +62,15 @@ function Login() {
                         <form>
                             <div class="mb-3">
                                 <label for="exampleInputEmail1" className="form-label">Usuario</label>
-                                <input type="email" className="form-control" placeholder="Usuario" />
+                                <input type="text" className="form-control" placeholder="Usuario" onChange={(e)=>{setLoginnombreusuario(e.target.value)}}/>
                             </div>
                             <div className="mb-3">
                                 <label for="exampleInputPassword1" className="form-label">Contrseña</label>
-                                <input type="password" className="form-control" placeholder="Contraseña" />
+                                <input type="password" className="form-control" placeholder="Contraseña" onChange={(e)=>{setLogincontraseña(e.target.value)}}/>
                             </div>
                             <div className="text-center">
                                 <button type="button" className="btn btn-secondary" onClick={()=>setModal(true)}>Registrarse</button>
-                                <NavLink id="Iniciosesion" className="btn btn-primary" to={"/Home"}>Iniciar sesión</NavLink>
+                                <NavLink type="button" id="Iniciosesion" className="btn btn-primary" onClick={Login} to={estado?'/home':'/'}>Iniciar sesión</NavLink>
                             </div>
                         </form>
                     </div>
@@ -40,20 +85,17 @@ function Login() {
                             <div className="modal-body">
                                 <div className="form-group">
                                     <label for="Usuario" className="col-form-label">Nombre usuario: </label>
-                                    <input type="text" className="form-control border-dark" name="usuario" id="usuario" placeholder="Nombre usuario" />
+                                    <input type="text" className="form-control border-dark" name="usuario" id="usuario" placeholder="Nombre usuario" onChange={(e)=>{setNombreusuario(e.target.value)}}/>
                                 </div>
                                 <div className="form-group">
                                     <label for="contraseña" className="col-form-label">Contraseña: </label>
-                                    <input type="password" className="form-control border-dark" name="contraseña" id="contraseña" placeholder="contrseña" />
+                                    <input type="password" className="form-control border-dark shadow-none" name="contraseña" id="contraseña" placeholder="contrseña" onChange={(e)=>{setContraseña(e.target.value)}}/>
                                 </div>
-                                <div className="form-group">
-                                    <label for="confircontraseña" className="col-form-label">Confirmar contraseña: </label>
-                                    <input type="password" className="form-control border-dark" name="confircontraseña" id="confircontraseña" placeholder="contraseña" />
-                                </div>
+                                <Indicador password={contraseña}/>
                             </div>
                             <div className="modal-footer">
                                 <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={()=>setModal(false)}>Cancelar</button>
-                                <button type="button" id="btnEditar" className="btn btn-primary" onClick={()=>setModal(false)}>Registrar</button>
+                                <button type="button" id="btnEditar" className="btn btn-primary" onClick={Registro}>Registrar</button>
                             </div>
                         </form>
                     </ModalBody>
--- Servidor/app.js
+++ Servidor/app.js
@@ -1,21 +1,87 @@
+//Importaciones
 const { request } = require('express');
 const express = require('express');
-const cors = require('cors');
+const cors=require("cors");
+const bcrypt=require("bcrypt");
+const bodyParser=require("body-parser");
+const cookieParser=require("cookie-parser");
+const session=require("express-session");
+const mysql = require('mysql2/promise');
+const bluebird = require('bluebird');
+//
+
+const saltRounds=10;
 const app = express();
 const port = 3001;
-const mysql = require('mysql2/promise');
 let connection;//variable para almacenar las conexiones a la base de datos
-const bluebird = require('bluebird');
 //configura el servidor para recibir datos en json
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:["http://localhost:3000"],
+    methods:["GET", "POST", "UPDATE"],
+    credentials:true
+}));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(session({
+    key:"id",
+    secret: "sercret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires:60*60*24,
+    }
+}))
 app.set('port', process.env.PORT || port);
+//endpoints
+app.get("/", (req, res) => {
+    res.json("Backend Login");
+});
+app.get('/login', (req, res)=>{
+    if(req.session.usuario){
+        res.json({loggedIn: true, usuario: req.session.usuario});
+    }else{
+        res.json({loggedIn: false});
+    }
+});
+app.post('/registro-usuario', async (req,res)=>{
+    const usuario=req.body;
+    bcrypt.hash(usuario.contraseña, saltRounds,async (err, hash)=>{
+        if(err)
+        {
+            res.json(err);
+        }
+        await connection.execute(`INSERT INTO usuarios (nombre , contraseña) VALUES ('${usuario.nombre}', '${hash}')`);
+    });
+    res.json("Usuario agregado");
+    console.log("Usuario agregado");
+});
+app.post('/login', async (req,res)=>{
+    const usuario=req.body;
+    const [rows, fields]=await connection.execute(`SELECT * FROM usuarios WHERE nombre='${usuario.nombre}'`);
+    if (rows[0] != undefined) {
+        bcrypt.compare(usuario.contraseña, rows[0].contraseña, (error, result)=>{
+            if(result){
+                req.session.usuario=rows;
+                console.log(req.session.usuario)
+                res.json(usuario);
+            }else{
+                res.json({ "Mensaje": "Usuario o contraseña equivocada" });
+                console.log("Datos incorrectos");
+            }
+        });
+    } else {
+        res.json({ "Mensaje": "Usuario no existe" });
+        console.log("Usuario no existe");
+    }
+});
+
 app.listen(app.get('port'), async () => {
     connection = await mysql.createConnection({
-        host: 'sql10.freesqldatabase.com',
-        user: 'sql10447201',
-        password: 'QiB7dv5A1A',
-        database: 'sql10447201',
+        host: 'localhost',
+        user: 'root',
+        password: '12345678',
+        database: 'usuarios',
         Promise: bluebird
     });
     console.log("Server running on port: " + app.get('port'));
